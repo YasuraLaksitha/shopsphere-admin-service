85f4c7afaaee89a7a289efbd2168f30663cf4c8b
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-06-07 11:23:52 +0530

    Entity and DTO config for Product and categories

--- src/main/java/com/shopsphere/admin_service/AdminServiceApplication.java
+++ src/main/java/com/shopsphere/admin_service/AdminServiceApplication.java
@@ -2,8 +2,10 @@ package com.shopsphere.admin_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/shopsphere/admin_service/audit/AuditAwareImpl.java
@@ -0,0 +1,15 @@
+package com.shopsphere.admin_service.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component(value = "auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("PRODUCTS_MS");
+    }
+}
